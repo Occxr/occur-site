e0db24ff363f9b4a2404be29ec124b4dd9b9a6b1
Occxr <louj3895@gmail.com>
2025-04-20 15:52:22 -0700

    Fix missing RegisterForm

--- app/forms.py
+++ app/forms.py
@@ -1,12 +1,13 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, DecimalField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired, Length, Email, Optional
+from wtforms import EmailField
 
 
-class SignupForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(min=3, max=25)])
+class RegisterForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=4, max=25)])
+    email = EmailField('Email (optional)', validators=[Optional()])
     password = PasswordField('Password', validators=[DataRequired(), Length(min=8)])
-    email = StringField('Email (optional)', validators=[Optional(), Email()])
     submit = SubmitField('Sign Up')
 
 
