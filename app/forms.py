03fdbab419c4779c6db02237127b60853a816a9f
Occxr <louj3895@gmail.com>
2025-04-20 13:36:15 -0700

    ✅ Add ProductForm to fix deployment error

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,17 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, DecimalField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired, Length, Email, Optional
+
 
 class SignupForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=3, max=25)])
     password = PasswordField('Password', validators=[DataRequired(), Length(min=8)])
     email = StringField('Email (optional)', validators=[Optional(), Email()])
     submit = SubmitField('Sign Up')
+
+
+class ProductForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    price = DecimalField('Price', validators=[DataRequired()])
+    description = TextAreaField('Description')
+    submit = SubmitField('Add Product')
