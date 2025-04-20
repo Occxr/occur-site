8612dc015351c46b36680c61005600d71bbbe929
Occxr <louj3895@gmail.com>
2025-04-20 13:40:18 -0700

    🧠 Add Flask-Login user_loader to fix session error

--- app/__init__.py
+++ app/__init__.py
@@ -12,6 +12,12 @@ login_manager = LoginManager()
 login_manager.login_view = 'auth.login'
 csrf = CSRFProtect()
 
+from .models import User
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 def create_app():
     app = Flask(__name__)
     app.config.from_object('config.Config')
